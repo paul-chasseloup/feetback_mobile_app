12b6286294c71272ae691e01c3275e9def0e3fdc
gladalle78 <33734800+gladalle78@users.noreply.github.com>
2020-03-04 12:14:58 +0100

    add chronometer

--- app/src/main/java/paulchasseloup/feetback_mobile_app/dataActivity.java
+++ app/src/main/java/paulchasseloup/feetback_mobile_app/dataActivity.java
@@ -2,9 +2,12 @@ package paulchasseloup.feetback_mobile_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -13,18 +16,23 @@ public class dataActivity extends AppCompatActivity {
 
     private ToggleButton mStartStopBtn;
     private TextView mDisconnect;
+    private Chronometer mChronometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
         mStartStopBtn = (ToggleButton) findViewById(R.id.toggleButton);
         mDisconnect = (TextView) findViewById(R.id.disconnectLink);
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
 
         mStartStopBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
+                    mChronometer.start();
                     // here the method to collect the data from the device
                 } else {
+                    mChronometer.setBase(SystemClock.elapsedRealtime());
                     // here the method to to stop the sampling and to send the DB
                 }
             }
